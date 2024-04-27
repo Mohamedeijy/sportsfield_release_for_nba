744b09252c9e50a302038d3d97f3616e80bde492
fallmoha <mohamed.fall@etu.utc.fr>
2024-04-27 20:41:12 +0200

    nba dataset

--- datasets/aligned_dataset.py
+++ datasets/aligned_dataset.py
@@ -7,6 +7,10 @@ import abc
 from torch.utils import data
 import imageio
 import tables
+import os
+import pandas as pd
+import numpy as np
+import ast
 
 from utils import utils, warp, image_utils
 
@@ -23,8 +27,8 @@ class AlignedDatasetFactory():
             dataset_type {[str]} -- [train/val/test]
         '''
 
-        if opt.dataset_name == 'world_cup':
-            dset = WorldCupAlignedDataset(opt, dataset_type)
+        if opt.dataset_name == 'nba':
+            dset = NBAAlignedDataset(opt, dataset_type)
         else:
             raise ValueError('unknown dataset: {0}'.format(opt.dataset_name))
         return dset
@@ -41,15 +45,20 @@ class AlignedDataset(data.Dataset, abc.ABC):
         assert dataset_type in ['train', 'val', 'test'], 'unknown dataset type {0}'.format(dataset_type)
         self.dataset_type = dataset_type
         self.load_template()
-        self.load_h5_file(dataset_type)
+        # self.load_h5_file(dataset_type)
+        self.load_dataframe(dataset_type)
+
+    # @abc.abstractmethod
+    # def load_h5_file(self, dataset_type):
+    #     pass
 
     @abc.abstractmethod
-    def load_h5_file(self, dataset_type):
+    def load_dataframe(self, dataset_type):
         pass
 
     def load_template(self):
         self.template_path = self.opt.template_path
-        self.template = imageio.imread(self.template_path, pilmode='RGB') / 255.0
+        self.template = imageio.v2.imread(self.template_path, pilmode='RGB') / 255.0
         if self.opt.coord_conv_template:
             self.template = image_utils.rgb_template_to_coord_conv_template(self.template)
 
@@ -62,18 +71,22 @@ class AlignedDataset(data.Dataset, abc.ABC):
 
     def __getitem__(self, index):
         img = self.get_image_by_index(index)
-        homography = self.get_homography_by_index(index)
-        warped_template = warp.warp_image(self.template, homography, out_shape=img.shape[-2:])[0]
-        return img, warped_template, homography
+        gt_homography = self.get_homography_by_index(index)
+        warped_template = warp.warp_image(self.template, gt_homography, out_shape=img.shape[-2:])[0]
+        return img, warped_template, gt_homography
 
     def get_homography_by_index(self, index):
-        homography = self.raw_data.root.homographies[index]
-        homography = utils.to_torch(homography)
-        homography = homography / homography[2:3, 2:3]
-        return homography
+        # homography = self.raw_data.root.homographies[index]
+        gt_homography = ast.literal_eval(self.df.loc[index, 'gt_homography'])
+        gt_homography = np.float32(gt_homography).flatten().reshape(3, 3)
+        gt_homography = utils.to_torch(gt_homography)
+        gt_homography = gt_homography / gt_homography[2:3, 2:3]
+        return gt_homography
 
     def get_image_by_index(self, index):
-        img = self.raw_data.root.frames[index]
+        # img = self.raw_data.root.frames[index]
+        img_path = os.path.join(self.root_dir_path, self.df.loc[index, 'image_path'])
+        img = imageio.v2.imread(img_path, pilmode='RGB')
         img = img[..., [2, 1, 0]]
         img = img / 255.0
         img = utils.to_torch(img).permute(2, 0, 1)
@@ -82,7 +95,7 @@ class AlignedDataset(data.Dataset, abc.ABC):
         return img
 
 
-class WorldCupDataset(abc.ABC):
+class NBADataset(abc.ABC):
     def load_h5_file(self, dataset_type):
         if dataset_type == 'test':
             self.dataset_path = self.opt.test_dataset_path
@@ -93,7 +106,13 @@ class WorldCupDataset(abc.ABC):
             self.raw_data.root.homographies)
         self.num_samples = len(self.raw_data.root.frames)
 
+    def load_dataframe(self, dataset_type):
+        self.root_dir_path = os.path.join(self.opt.root_dir_path, dataset_type)
+        print(f"root dir path: {self.root_dir_path}")
+        self.df = pd.read_csv(os.path.join(self.root_dir_path, 'annotations_' + dataset_type + '.csv'))
+        self.num_samples = len(self.df)
 
-class WorldCupAlignedDataset(WorldCupDataset, AlignedDataset):
+
+class NBAAlignedDataset(NBADataset, AlignedDataset):
     def __init__(self, opt, dataset_type):
         super().__init__(opt, dataset_type)
--- options/options.py
+++ options/options.py
@@ -11,7 +11,6 @@ import argparse
 import json
 import os
 
-
 from options.options_check import check_cuda, check_pretrained_weights, check_warp_params
 from options.options_utils import str2bool, print_opt, confirm_opt
 
@@ -38,32 +37,89 @@ def set_general_arguments(parser):
 
 def set_data_arguments(parser):
     data_arg = parser.add_argument_group('Data')
-    data_arg.add_argument('--dataset_name', choices=['world_cup'],
-                          type=str, default='world_cup', help='dataset name')
+    data_arg.add_argument('--dataset_name', choices=['nba'],
+                          type=str, default='nba', help='dataset name')
     data_arg.add_argument('--coord_conv_template', type=str2bool,
                           default=True, help='replace redundant channels tp XY grids')
     data_arg.add_argument('--template_path', type=str,
-                          default='./data/world_cup_template.png', help='playfield template image')
+                          default='./data/nba_template.png', help='playfield template image')
     data_arg.add_argument('--need_single_image_normalization', type=str2bool,
                           default=True, help='normalize a single image')
 
 
 def set_warp_arguments(parser):
     warp_arg = parser.add_argument_group('Warp')
-    warp_arg.add_argument('--warp_type', choices=['homography'], type=str, default='homography', help='how to warp images')
-    warp_arg.add_argument('--homo_param_method', choices=['deep_homography'], type=str, default='deep_homography', help='how to parameterize homography')
+    warp_arg.add_argument('--warp_type', choices=['homography'], type=str, default='homography',
+                          help='how to warp images')
+    warp_arg.add_argument('--homo_param_method', choices=['deep_homography'], type=str, default='deep_homography',
+                          help='how to parameterize homography')
 
 
 def set_dataset_paths(opt, global_config):
-    if opt.dataset_name == 'world_cup':
-        opt.test_dataset_path = global_config[opt.dataset_name]['test_dataset']
+    if opt.dataset_name == 'nba':
+        opt.root_dir_path = global_config[opt.dataset_name]['dataset']
+        # if opt.mode == 'test':
+        opt.test_dataset_path = os.path.join(global_config[opt.dataset_name]['dataset'], 'test')
     else:
         raise ValueError('unknown dataset_name: {0}'.format(opt.dataset_name))
     return opt
 
 
-def set_end2end_optim_options():
+def set_init_guesser_options():
+    global_config = read_global_config()
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--test_mode', type=str2bool,
+                        default=False, help='test mode')
+    args, _ = parser.parse_known_args()
+    training = not args.test_mode
+    set_general_arguments(parser)
+    set_data_arguments(parser)
+    set_warp_arguments(parser)
+
+    parser.add_argument('--guess_model', choices=['init_guess'], default='init_guess', help='type of initial guess network')
+    parser.add_argument('--train_epochs', type=int, default=15, help='number of epochs for training')
+    parser.add_argument('--prevent_neg', choices=['sigmoid'],
+                        type=str, default='sigmoid', help='how to prevent negative error estimation')
+    parser.add_argument('--imagenet_pretrain', type=str2bool, default=True,)
+    parser.add_argument('--load_weights_upstream', type=str, default=None,
+                        help='load a pretrained set of weights for upstream initial guesser, you need to provide the model name')
+    parser.add_argument('--out_dir', type=str,
+                        default=global_config['out'], help='out directory')
+    parser.add_argument('--need_spectral_norm_upstream', type=str2bool, default=False,
+                        help='apply spectral norm to conv layers, for upstream initial guesser')
+    parser.add_argument('--group_norm_upstream', type=int,
+                        default=0, help='0 is batch norm, otherwise means number of groups')
+    parser.add_argument('--optim_method', default='directh', choices=[
+        'stn', 'directh'], help='optimization method')
+    parser.add_argument('--optim_type', default='adam',
+                        choices=['adam', 'sgd'], help='gradient descent optimizer type')
+    parser.add_argument('--directh_part', default='six', choices=[
+        'full', 'lower', 'six'], help='optimize the lower 4 corners, or full 4 corners, or 6 corners')
+    parser.add_argument('--lr', type=float,
+                        default=1e-4, help='training learning rate')
+    parser.add_argument('--batch_size', type=int, default=16,
+                        help='batch size for training')
+
+    opt = parser.parse_args()
+    opt.command = ' '.join(sys.argv)
+    opt.training = bool(training)
+    opt.mode = 'train' if opt.training else 'test'
+
+    opt = set_dataset_paths(opt, global_config)
+    check_cuda(opt)
+    check_warp_params(opt)
+    check_pretrained_weights(opt)
+
+    if opt.confirm:
+        confirm_opt(opt)
+    else:
+        print_opt(opt)
+
+    return opt
+
+
+def set_end2end_optim_options():
     global_config = read_global_config()
 
     parser = argparse.ArgumentParser()
@@ -94,11 +150,11 @@ def set_end2end_optim_options():
     parser.add_argument('--group_norm_error_model', type=int,
                         default=0, help='0 is batch norm, otherwise means number of groups')
     parser.add_argument('--optim_method', default='directh', choices=[
-                        'stn', 'directh'], help='optimization method')
+        'stn', 'directh'], help='optimization method')
     parser.add_argument('--optim_type', default='adam',
                         choices=['adam', 'sgd'], help='gradient descent optimizer type')
     parser.add_argument('--directh_part', default='lower', choices=[
-                        'full', 'lower'], help='optimize the lower 4 corners, or full 4 corners')
+        'full', 'lower', 'six'], help='optimize the lower 4 corners, or full 4 corners, or 6 corners')
     parser.add_argument('--optim_criterion', default='l1loss',
                         choices=['l1loss', 'mse'], help='criterion for optimization')
     parser.add_argument('--lr_optim', type=float,
@@ -108,8 +164,9 @@ def set_end2end_optim_options():
     parser.add_argument('--batch_size', type=int, default=1,
                         help='batch size for optimization')
     parser.add_argument('--iou_space', default='part_and_whole', choices=[
-                        'model_whole', 'model_part', 'part_and_whole'], help='space for iou calculation')
-    parser.add_argument('--error_target', choices=['iou_whole'], type=str, default='iou_whole', help='error target for loss surface')
+        'model_whole', 'model_part', 'part_and_whole'], help='space for iou calculation')
+    parser.add_argument('--error_target', choices=['iou_whole'], type=str, default='iou_whole',
+                        help='error target for loss surface')
 
     opt = parser.parse_args()
     opt.command = ' '.join(sys.argv)
