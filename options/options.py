94599c49d673b22a287598d4e41529ef05d06107
fallmoha <mohamed.fall@etu.utc.fr>
2024-04-28 05:53:31 +0200

    loss surface options

--- options/options.py
+++ options/options.py
@@ -119,6 +119,65 @@ def set_init_guesser_options():
     return opt
 
 
+def set_loss_surface_options():
+    global_config = read_global_config()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--test_mode', type=str2bool,
+                        default=False, help='test mode')
+    args, _ = parser.parse_known_args()
+    training = not args.test_mode
+    set_general_arguments(parser)
+    set_data_arguments(parser)
+    set_warp_arguments(parser)
+
+    parser.add_argument('--error_model', choices=['loss_surface'], default='loss_surface',help='type of network')
+    parser.add_argument('--guess_model', choices=['init_guess'], default='init_guess', help='type of initial guess network')
+    parser.add_argument('--train_epochs', type=int, default=1, help='number of epochs for training')
+    parser.add_argument('--prevent_neg', choices=['sigmoid'],
+                        type=str, default='sigmoid', help='how to prevent negative error estimation')
+    parser.add_argument('--imagenet_pretrain', type=str2bool, default=False,)
+    parser.add_argument('--load_weights_upstream', type=str, default=None,
+                        help='load a pretrained set of weights for upstream initial guesser, you need to provide the model name')
+    parser.add_argument('--out_dir', type=str,
+                        default=global_config['out'], help='out directory')
+    parser.add_argument('--need_spectral_norm_upstream', type=str2bool, default=False,
+                        help='apply spectral norm to conv layers, for upstream initial guesser')
+    parser.add_argument('--group_norm_upstream', type=int,
+                        default=0, help='0 is batch norm, otherwise means number of groups')
+    parser.add_argument('--optim_method', default='directh', choices=[
+        'stn', 'directh'], help='optimization method')
+    parser.add_argument('--optim_type', default='adam',
+                        choices=['adam', 'sgd'], help='gradient descent optimizer type')
+    parser.add_argument('--directh_part', default='six', choices=[
+        'full', 'lower', 'six'], help='optimize the lower 4 corners, or full 4 corners, or 6 corners')
+    parser.add_argument('--lr', type=float,
+                        default=1e-4, help='training learning rate')
+    parser.add_argument('--batch_size', type=int, default=12,
+                        help='batch size for training')
+    parser.add_argument('--iou_space', default='part_and_whole', choices=[
+        'model_whole', 'model_part', 'part_and_whole'], help='space for iou calculation')
+    parser.add_argument('--error_target', choices=['iou_whole'], type=str, default='iou_whole',
+                    help='error target for loss surface')
+
+    opt = parser.parse_args()
+    opt.command = ' '.join(sys.argv)
+    opt.training = bool(training)
+    opt.mode = 'train' if opt.training else 'test'
+
+    opt = set_dataset_paths(opt, global_config)
+    check_cuda(opt)
+    check_warp_params(opt)
+    check_pretrained_weights(opt)
+
+    if opt.confirm:
+        confirm_opt(opt)
+    else:
+        print_opt(opt)
+
+    return opt
+
+
 def set_end2end_optim_options():
     global_config = read_global_config()
 
@@ -131,8 +190,8 @@ def set_end2end_optim_options():
     set_data_arguments(parser)
     set_warp_arguments(parser)
 
-    parser.add_argument('error_model', choices=['loss_surface'], help='type of network')
-    parser.add_argument('guess_model', choices=['init_guess'], help='type of initial guess network')
+    parser.add_argument('--error_model', choices=['loss_surface'], help='type of network')
+    parser.add_argument('--guess_model', choices=['init_guess'], help='type of initial guess network')
     parser.add_argument('--prevent_neg', choices=['sigmoid'],
                         type=str, default='sigmoid', help='how to prevent negative error estimation')
     parser.add_argument('--load_weights_upstream', type=str, default=None,
